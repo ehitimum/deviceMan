6edd917201f26165640619aa6773be85d4c31249
Ehtimam <70962870+Ehtimam@users.noreply.github.com>
2023-04-07 21:47:07 +0600

    ADD: Initial Commit 0.0

---
+++ api/controllers/company.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from api.repositories.company_repository import create_company
+
+@require_POST
+def create_company(request):
+    company_name = request.POST.get('company_name')
+    company_address = request.POST.get('company_address')
+
+    # Create a new company in the database
+    company_id = create_company(company_name, company_address)
+
+    # Create the JSON response
+    response = {
+        'company_id': company_id,
+        'company_name': company_name,
+        'company_address': company_address,
+        'message': 'Company created successfully'
+    }
+
+    return JsonResponse(response)
