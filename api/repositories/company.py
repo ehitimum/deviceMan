cf35e2fdc2878ab9bb844e572677d6a9787769b2
Ehtimam <70962870+Ehtimam@users.noreply.github.com>
2023-04-07 22:20:38 +0600

    ADD: commit 0.2

--- api/repositories/company.py
+++ api/repositories/company.py
@@ -1,20 +1,7 @@
 from django.db import IntegrityError
 from api.models.company import Company
 
-def create_company(name: str, address: str) -> dict:
-    """
-    Create a new company and return its details as a dictionary.
-    """
-    try:
-        company = Company.objects.create(name=name, address=address)
-    except IntegrityError:
-        # Handle duplicate key error
-        return {'error': 'Company already exists.'}
-    
-    # Return company details as a dictionary
-    
-    return {
-        'id': company.id,
-        'name': company.name,
-        'address': company.address,
-    }
+def create_company(name, address):
+    company = Company(name=name, address=address)
+    company.save()
+    return company.id
